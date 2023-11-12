16bb2ea2fcd8d92f2718e9c9ee9649a9b5aec7a0
xunuwu <xunuwu@gmail.com>
2023-11-12 02:22:01 +0100

    small syncthing changes

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -3,9 +3,9 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    settings.options.urAccepted = -1; # disable usage reporting
     overrideDevices = true;
     overrideFolders = true;
+
     settings.devices = {
       "nixdesk" = {
         id = "2WCEQPF-2J4U7IK-XRT25FV-NFT2JEM-AVOMDEK-FIJNZ24-7WCBZC2-57CX2AP";
@@ -14,6 +14,10 @@
         id = "U6YYTHR-2ZXIEXQ-RNDERSF-CTVSP67-W24VK4Y-5EQRIV5-T7JJW2N-33L7XQV";
       };
     };
+
+    settings.options.urAccepted = -1; # disable usage reporting
     settings.options.globalAnnounceEnabled = false; # only sync on LAN
+    settings.gui.insecureSkipHostcheck = true;
+    settings.gui.insecureAdminAccess = true;
   };
 }
