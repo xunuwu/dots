622d8867d315106089163960bfa6d5ef1f365519
xunuwu <xunuwu@gmail.com>
2023-11-12 20:14:25 +0100

    more small syncthing changes

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -9,17 +9,14 @@
       "nixdesk" = {
         id = "2WCEQPF-2J4U7IK-XRT25FV-NFT2JEM-AVOMDEK-FIJNZ24-7WCBZC2-57CX2AP";
         autoAcceptFolders = true;
-        addresses = [ "tcp://192.168.50.26:51820" "tcp://nixdesk:51820"];
       };
       "redmi-note-10-pro" = {
         id = "U6YYTHR-2ZXIEXQ-RNDERSF-CTVSP67-W24VK4Y-5EQRIV5-T7JJW2N-33L7XQV";
         autoAcceptFolders = true;
-        addresses = [ "tcp://192.168.50.26:51820" "tcp://redmi-note-10-pro:51820"];
       };
       "hopper" = {
         id = "DK3RPET-ACMULD2-TLQS6YM-XWUMS3N-JRNDNME-YTM3H4X-P7QVUKB-N3PL5QF";
         autoAcceptFolders = true;
-        addresses = [ "tcp://192.168.50.97:51820" "tcp://hopper:51820"];
       };
     };
 
