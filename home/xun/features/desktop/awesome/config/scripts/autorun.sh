aa5863c9cf96ec016666c1241f0533022ff17b99
xunuwu <xunuwu@gmail.com>
2023-11-25 19:19:58 +0100

    add gnome polkit

--- home/xun/features/desktop/awesome/config/scripts/autorun.sh
+++ home/xun/features/desktop/awesome/config/scripts/autorun.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 xrandr --output DP-3 --mode 1920x1080 --rate 165 &
-#/nix/store/$(ls -la /nix/store | grep polkit-gnome | grep '^d' | awk '{print $9}')/libexec/polkit-gnome-authentication-agent-1 &
+/nix/store/$(ls -la /nix/store | grep polkit-gnome | grep '^d' | awk '{print $9}')/libexec/polkit-gnome-authentication-agent-1 &
