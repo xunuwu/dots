1253fc372ad5d95cdae84348a2ce484ad32a4579
xunuwu <xunuwu@gmail.com>
2023-12-05 21:55:31 +0100

    wezterm colorscheme

--- home/xun/features/desktop/common/wezterm.nix
+++ home/xun/features/desktop/common/wezterm.nix
@@ -3,14 +3,9 @@
   pkgs,
   lib,
   ...
-}: let
-  inherit (config) colorscheme;
-  inherit (colorscheme) colors;
-  l = lib;
-in {
+}: {
   programs.wezterm = {
     enable = true;
-
     extraConfig = ''
       local wezterm = require'wezterm'
       local c = {}
@@ -18,7 +13,7 @@ in {
         c = wezterm.config_builder()
       end
       c.hide_tab_bar_if_only_one_tab = true
-      c.color_scheme = "catppuccin-mocha"
+      c.color_scheme = "GitHub Dark"
       c.window_padding = { left = 10, right = 10, top = 5, bottom = 5 }
       c.window_decorations = 'RESIZE'
       c.window_close_confirmation = "NeverPrompt"
