606db45039d7ff26f1c3beff6fc411482fadceb1
xunuwu <xunuwu@gmail.com>
2023-12-06 01:09:42 +0100

    vpn things

--- hosts/nixdesk/services/vpn/default.nix
+++ hosts/nixdesk/services/vpn/default.nix
@@ -1,7 +1,17 @@
 {config, ...}: {
-  networking.wg-quick.interfaces."wg0".configFile = config.sops.secrets.vpn-conf.path;
-  sops.secrets.vpn-conf = {
+  networking.wg-quick.interfaces."wg0".configFile = config.sops.secrets.wireguard.path;
+  #services.openvpn.servers = {
+  #  mainVPN = {
+  #    config = "config " + config.sops.secrets.openvpn.path;
+  #  };
+  #};
+
+  sops.secrets.wireguard = {
     format = "binary";
-    sopsFile = ./config;
+    sopsFile = ./wireguard;
+  };
+  sops.secrets.openvpn = {
+    format = "binary";
+    sopsFile = ./openvpn;
   };
 }
